212cc8e91265b8bb89cfe505bdebb8fabc9b6cc7
Marc-Andre Quintal <maquintal16@gmail.com>
2021-06-14 20:13:21 -0400

    nextjs + material-ui with theme

---
+++ my-app/pages/index.js
@@ -0,0 +1,11 @@
+import styles from '../styles/Home.module.css'
+
+import ClippedDrawer from '../src/clippedDrawer'
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <ClippedDrawer />
+    </div>
+  )
+}
