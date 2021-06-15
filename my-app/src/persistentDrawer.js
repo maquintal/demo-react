e21dce148b19229441a20d17ed62f5393e135145
Marc-Andre Quintal <maquintal16@gmail.com>
2021-06-14 22:01:31 -0400

    exploration <Component /> into drawer

--- my-app/src/persistentDrawer.js
+++ my-app/src/persistentDrawer.js
@@ -1,13 +1,9 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import Head from 'next/head';
-import { ThemeProvider } from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
-// import theme from '../src/theme';
+
 import clsx from 'clsx';
 import { makeStyles, useTheme } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-// import CssBaseline from '@material-ui/core/CssBaseline';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
@@ -82,9 +78,7 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function MyApp(props) {
-  const { Component, pageProps } = props;
-
+export default function PersistentDrawerLeft() {
   const classes = useStyles();
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
@@ -97,24 +91,7 @@ export default function MyApp(props) {
     setOpen(false);
   };
 
-  React.useEffect(() => {
-    // Remove the server-side injected CSS.
-    const jssStyles = document.querySelector('#jss-server-side');
-    if (jssStyles) {
-      jssStyles.parentElement.removeChild(jssStyles);
-    }
-  }, []);
-  
-
   return (
-    <React.Fragment>
-      <Head>
-        <title>My page</title>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-
     <div className={classes.root}>
       <CssBaseline />
       <AppBar
@@ -177,7 +154,6 @@ export default function MyApp(props) {
         })}
       >
         <div className={classes.drawerHeader} />
-        <Component {...pageProps} />
         {/* <Typography paragraph>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
           ut labore et dolore magna aliqua. Rhoncus dolor purus non enim praesent elementum
@@ -203,13 +179,5 @@ export default function MyApp(props) {
         </Typography> */}
       </main>
     </div>
-
-      </ThemeProvider>
-    </React.Fragment>
   );
 }
-
-MyApp.propTypes = {
-  Component: PropTypes.elementType.isRequired,
-  pageProps: PropTypes.object.isRequired,
-};
