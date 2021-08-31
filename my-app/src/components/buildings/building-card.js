62f152a3fcc887e599588057468ef640a276c116
FrancisCampeau <frank.camps19@gmail.com>
2021-08-31 12:35:31 -0400

    adding accordion to cards

--- my-app/src/components/buildings/building-card.js
+++ my-app/src/components/buildings/building-card.js
@@ -10,6 +10,9 @@ import {
   IconButton,
   Typography,
   Avatar,
+  AccordionSummary,
+  AccordionDetails,
+  Accordion,
 } from "@material-ui/core";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import FavoriteIcon from "@material-ui/icons/Favorite";
@@ -21,26 +24,8 @@ import clsx from "clsx";
 import { FixedSizeList } from "react-window";
 import PropTypes from "prop-types";
 
-function renderRow({props}) {
-  const { index, style } = props;
-
-  return (
-    <ListItem button style={style} key={index}>
-      <ListItemText primary={`Appartement # ${index + 1}`} />
-    </ListItem>
-  );
-}
-
-renderRow.propTypes = {
-  index: PropTypes.number.isRequired,
-  style: PropTypes.object.isRequired,
-};
-
-const BuildingCard = ({ building, classes, expanded, handleExpandClick }) => {
-  
-    const {formData} = building
-    
-
+const BuildingCard = ({ building, classes }) => {
+  const { formData } = building;
 
   return (
     <>
@@ -59,7 +44,7 @@ const BuildingCard = ({ building, classes, expanded, handleExpandClick }) => {
                 <MoreVertIcon />
               </IconButton>
             }
-            title={`${formData.civicNumber} ${formData.street}, ${formData.city}, ${formData.zipCode}`}
+            title={`${formData.civicNumber} ${formData.street}, ${formData.city}, ${formData.zip_code}`}
             subheader={`${formData.rental_agent}`}
           />
           <CardMedia
@@ -90,33 +75,26 @@ const BuildingCard = ({ building, classes, expanded, handleExpandClick }) => {
             <IconButton aria-label="share" color="secondary">
               <DeleteIcon />
             </IconButton>
-
-            <IconButton
-              className={clsx(classes.expand, {
-                [classes.expandOpen]: expanded,
-              })}
-              onClick={handleExpandClick}
-              aria-expanded={expanded}
-              aria-label="show more"
-            >
-              <ExpandMoreIcon />
-            </IconButton>
             <Typography variant="body2" color="textSecondary" component="p">
               Appartements
             </Typography>
           </CardActions>
-          <Collapse in={expanded} timeout="auto" unmountOnExit>
-            <CardContent>
-              <FixedSizeList
-                height={400}
-                width={300}
-                itemSize={46}
-                itemCount={200}
+          <CardContent>
+            <Accordion>
+              <AccordionSummary
+                expandIcon={<ExpandMoreIcon />}
+                aria-controls="panel1a-content"
+                id="panel1a-header"
               >
-                {renderRow}
-              </FixedSizeList>
-            </CardContent>
-          </Collapse>
+                <Typography className={classes.heading}>Appartements</Typography>
+              </AccordionSummary>
+              <AccordionDetails>
+                <Typography>
+                  Ici ajouter une LIST avec les appartements a afficher
+                </Typography>
+              </AccordionDetails>
+            </Accordion>
+          </CardContent>
         </Card>
       </Grid>
     </>
