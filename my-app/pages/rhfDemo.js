627cc6a5434861b9be3ce37e52c66d6885d3ab8e
FrancisCampeau <frank.camps19@gmail.com>
2021-09-23 23:00:45 -0400

    workout session

--- my-app/pages/rhfDemo.js
+++ my-app/pages/rhfDemo.js
@@ -1,12 +1,40 @@
-import React from "react"
+import React, {useEffect} from "react"
+import { useDispatch, useSelector } from "react-redux"
+import axios from 'axios'
 
 import ReactHookFormDemo from "../src/react-hook-form-demo"
 
-const RHFDemo = () => {
+import { jusDePeche } from "../src/store/rootSlice"
+
+const RHFDemo = ({buildings}) => {
+
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    if (buildings) {
+      dispatch(jusDePeche(buildings))
+    }
+  }, [])
+
 
   return (<div>
     <ReactHookFormDemo />
   </div>)
 }
+
+RHFDemo.getInitialProps = async (ctx) => {
+  try {
+    const res = await axios.get(
+      "http://localhost:3000/api/buildings/readAllBuildings"
+    );
+    console.log('res', res.data);
+    const buildings = res.data;
+    return { buildings };
+  } catch (error) {
+    return { error };
+  }
+};
+
+
 
 export default RHFDemo
