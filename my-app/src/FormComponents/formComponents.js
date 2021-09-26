49b7c01f9d426c1ca2adf9297a6a357df69ede32
Marc-Andre Quintal <maquintal16@gmail.com>
2021-09-26 15:18:54 -0400

    add input error handling

--- my-app/src/FormComponents/formComponents.js
+++ my-app/src/FormComponents/formComponents.js
@@ -1,19 +1,36 @@
 import { TextField } from "@material-ui/core";
+import { isDraft } from "immer";
 import { useController } from "react-hook-form";
 
-export function Input({ control, name, label, type }) {
+export function Input({ control, name, label, type, rules }) {
   const {
     field: { ref, ...inputProps },
     fieldState: { invalid, isTouched, isDirty, error },
-    formState: { touchedFields, dirtyFields }
+    formState: { touchedFields, dirtyFields, isSubmitted }
   } = useController({
     name,
     control,
     // rules: { required: true },
+    rules: { rules },
     defaultValue: "",
   });
 
-  console.log(error)
+  /**
+   * The Error Module from RHF is not working properly with the useController mode
+   * this is the reason why we need to create our own error handling system
+   *  
+   * the error handling module returns true or false, by default it returns false
+   * to be returned true, if the component must meet thoses 3 conditions
+   * 
+   * isSubmitted
+   * rules?.required !== false
+   * inputProps.value === ""
+   * 
+   * */
+
+  const displayError = (isSubmitted && rules?.required !== false && inputProps.value === "") || false
+  const displayHelperText = displayError ? rules?.required : ""
+
   return (
     <TextField
       fullWidth
@@ -22,6 +39,8 @@ export function Input({ control, name, label, type }) {
       variant={"outlined"}
       label={label}
       type={type}
+      error={displayError}
+      helperText={displayHelperText}
     />
   )
 } 
